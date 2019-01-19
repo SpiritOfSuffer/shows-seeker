10f407170d9f83ca25fe447fc5c88277a4cfe6fb
Top Deuse <topdeuse@gmail.com>
2019-01-19 22:47:10 +0300

    Frontend init(vue)

--- index.js
+++ index.js
@@ -16,8 +16,16 @@ app.use(cors());
 // @desc    Get Shows By Title
 // @access  Public
 app.get('/v1/api', async (req, res) => {
-    let title = req.query.title;
-    let options = {
+    const title = req.query.title;
+    var genre;
+
+    if(req.query.genre){
+        genre = req.query.genre;
+        var genreId = getGenreId(genre);
+        console.log("id is " + genreId);
+    }
+    
+    const options = {
         method: 'POST',
         url: API,
         headers : {
@@ -32,13 +40,39 @@ app.get('/v1/api', async (req, res) => {
             "id": 1
           }
         };
-
-    request(options, async (error, response, body) => {
+    
+    await request(options, async (error, response, body) => {
         if(!error && response.statusCode == 200){
             console.log(body.result.length);
+            console.log(genreId);
 
             var data = [];
-            for(let i = 0; i < body.result.length; i++) {
+            for(var i = 0; i < body.result.length; i++) {
+                if(genreId) {
+                    
+                    console.log(genreId);
+                    console.log(body.result[i].genreIds);
+                    for(var j = 0; j < body.result[i].genreIds.length; i++) {
+                        if(genreId == body.result[i].genreIds[j]) {
+                            data.push(
+                                { 
+                                    id: body.result[i].id,
+                                    title: body.result[i].title,
+                                    titleOriginal: body.result[i].titleOriginal,
+                                    description: body.result[i].description,
+                                    totalSeasons: body.result[i].totalSeasons,
+                                    country: body.result[i].country,
+                                    started: body.result[i].started,
+                                    year: body.result[i].year,
+                                    rating: body.result[i].rating,
+                                    image: body.result[i].image,
+                                    channel: body.result[i].network.title,
+                                    genreIds: body.result[i].genreIds
+                                 }
+                            );
+                        }
+                    }
+                }
                 data.push(
                     { 
                         id: body.result[i].id,
@@ -51,22 +85,29 @@ app.get('/v1/api', async (req, res) => {
                         year: body.result[i].year,
                         rating: body.result[i].rating,
                         image: body.result[i].image,
-                        channel: body.result[i].network.title
+                        channel: body.result[i].network.title,
+                        genreIds: body.result[i].genreIds
                      }
                 );
             }
+            //res.send(await getShowsByGenre(req.query.genre, data));
+            
             res.send(data);
         }
-        else {
+        else{
             res.send({ success: "false" });
         }
     });
 
 });
 
+
+// @route   GET /v1/api/top
+// @desc    Get Top of Shows By Count
+// @access  Public
 app.get('/v1/api/top', async (req, res) => {
-    let count = req.query.count;
-    let options = {
+    const count = req.query.count;
+    const options = {
         method: 'POST',
         url: API,
         headers : {
@@ -83,17 +124,114 @@ app.get('/v1/api/top', async (req, res) => {
           }
         };
 
-    request(options, async (error, response, body) => {
-        if(!error && response.statusCode == 200) {
+    await request(options, async (error, response, body) => {
+        if(!error && response.statusCode == 200){
             res.send(body);
+            getGenreId("Криминал");
         }
-        else {
+        else{
             res.send({ success: "false" });
         }
     });   
 
 
 });
+
+
+
+
+app.get('/v1/api/genres', async (req, res) => {
+    const genre = req.query.genre;
+    console.log(genre);
+    const options = {
+        method: 'POST',
+        url: API,
+        headers : {
+            'Content-Type' : 'application/json'
+        },
+        json: {
+            "jsonrpc": "2.0",
+            "method": "shows.Top",
+            "params": {
+              "mode": "all",
+              "count": 500
+            },
+            "id": 1
+          }
+        };
+
+    await request(options, async (error, response, body) => {
+        if(!error && response.statusCode == 200){
+            var genreId = await getGenreId(genre);
+            var data = [];
+            //res.send(body);
+            for(var i = 0; i < body.result.length; i++) {
+                console.log("ids below");
+                console.log(body.result[i]);
+                for(var j = 0; j < body.result[i].genreIds.length; i++) {
+                    if(genreId == body.result[i].genreIds[j]) {
+                        data.push(
+                            { 
+                                id: body.result[i].id,
+                                title: body.result[i].title,
+                                titleOriginal: body.result[i].titleOriginal,
+                                description: body.result[i].description,
+                                totalSeasons: body.result[i].totalSeasons,
+                                country: body.result[i].country,
+                                started: body.result[i].started,
+                                year: body.result[i].year,
+                                rating: body.result[i].rating,
+                                image: body.result[i].image,
+                                channel: body.result[i].network.title,
+                             }
+                        );
+                    }
+                }
+                
+            }
+            res.send(data);
+        }
+        else{
+            res.send({ success: "false" });
+        }
+    });       
+});
+
+async function getGenreId(genre){
+    const options = {
+        method: 'POST',
+        url: API,
+        headers : {
+            'Content-Type' : 'application/json'
+        },
+        json: {
+            "jsonrpc": "2.0",
+            "method": "shows.Genres",
+            "params": {},
+            "id": 1
+        }
+    };
+
+    await request(options, async (error, response, body) => {  
+        if(!error && response.statusCode == 200){
+            var genreId;
+
+            for(var i = 0; i < body.result.length; i++) {
+                if(body.result[i].title == genre) {
+                    var id = body.result[i].id;
+                }
+                genreId = id;
+            }
+            
+            console.log("in returning")
+            return genreId;
+            //res.send(shows);
+        }
+        else{
+            res.send({ success: "false" });
+        }
+    }); 
+}
 
 app.listen(port, () => { 
     console.log(`Server is up on ${port}`);
